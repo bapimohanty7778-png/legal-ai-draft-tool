527ee9da54e121bf954b18a63633f5d9737c71fb
bapimohanty7778-png <bapimohanty7778@gmail.com>
2026-02-11 11:26:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 
 # ---------- Configure API ----------
-genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
@@ -42,5 +42,6 @@ if draft:
             file_name="improved_draft.txt",
             mime="text/plain"
         )
+
 
 
